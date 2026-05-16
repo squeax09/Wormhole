b4b8c80af29b54174509079d7394a4232051289c
tzerolalala <131914786+tzerolalala@users.noreply.github.com>
2026-05-16 14:24:28 +0800

    Chinese translation
    Chinese translation
    I translated all the new content, except for the team introduction :)

---
+++ localization/lancer_fan_club/zh_CN.lua
@@ -0,0 +1,394 @@
+return {
+    descriptions = {
+        PotatoPatch = {
+            PotatoPatchTeam_lancer_fan_club = { name = "Lancer Fan Club" },
+
+            PotatoPatchDev_ProdByProto = {
+                name = "ProdByProto",
+                text = {
+                    {
+                        "{s:2} {}",
+                        "There's at least {s:1.5,C:attention}a few{} of us",
+                        "Most of us go by the names Ash or Memphis, interchangeably.",
+                        "One of us chose not to have a name.",
+                        "It/They | Plural"
+                    },
+                    {
+                        "Made the music for {C:attention}Eigengrau Emptiness{}, came up with initial ideas for",
+                        "{C:attention}Hitchhiker's Guide{} and {C:attention} Moon Berry{}. {C:inactive,s:0.85}Did some other things as well.",
+                        "Also did programming, hunting for the sfx, and art {s:1.25}direction{} for moon berry.",
+                        "Pretty proud of all the details I added in the way the joker works.",
+                        "{s:0.5} {}",
+                        "{C:inactive}It looks like you're trying to seal the {C:dark_edition,E:1}Dark Fountain{C:inactive}, would you like some help?",
+                        "{C:inactive}Make sure to thank j8-bit for making Clippy!Ralsei so cute!"
+                    },
+                    {
+                        "{C:dark_edition,E:2,s:1.75}Trials Of The Protogen {C:attention}coming soon!",
+                        "Lots of challenges and achievements to be had.",
+                        "{s:0.5} ",
+                        "Shoutouts to {C:lfc_trans_blue}T{C:lfc_trans_pink}r{}a{C:lfc_trans_pink}n{C:lfc_trans_blue}s {C:lfc_fox}Foxgirls!",
+                        "{C:dark_edition,E:1}Click on Clippy!Ralsei to make a thing happen",
+                        "{C:dark_edition,E:1,s:0.75}Click 5 times for something else"
+                    },
+                    {
+                        "So what's the deal with spaceport food?",
+                    }
+                },
+            },
+
+            PotatoPatchDev_ellestuff = {
+                name = "ellestuff.",
+                text = {
+                    {
+                        "{s:1.5,C:lfc_ash}8{s:1.5} of us and counting!",
+                        "{s:0.6,C:lfc_elle}elle.{s:0.6} | {s:0.6,C:green}Amber{s:0.6} | Emily | {s:0.6,C:planet}Umbra{s:0.6} | {s:0.6,C:blue}Sara{s:0.6} | {s:0.6,C:legendary}Suzy{s:0.6} | {s:0.6,C:lfc_ash}Ash{s:0.6} | {s:0.6,C:money}Rebecca",
+                        "{s:0.9}She/Her | Plural"
+                    },
+                    {
+                        "{s:0.9}We made the code for the {s:0.9,C:lfc_meteor}Meteors{s:0.9},",
+                        "{s:0.9,C:rare}Spacebar{s:0.9}, and {s:0.9,C:uncommon}Astronaut Food{s:0.9},",
+                        "{s:0.9}along with the shaders for the",
+                        "{s:0.9,C:attention}Credits {s:0.9}and {s:0.9,C:uncommon}Urination Station",
+                    },
+                    {
+                        "If you're interested in",
+                        "more of our stuff:",
+                        "{s:0.8,X:blue,C:white}Bluesky{s:0.8} @ellestuff.dev",
+                        "{s:0.8,X:lfc_discord,C:white}Discord{s:0.8} ellestuff.dev/discord",
+                        "{s:0.8,X:lfc_dark,C:white}GitHub{s:0.8} @ellestuff",
+                        "{s:0.8}Play our mod {s:0.8,C:lfc_elle}ellejokers.{s:0.8},",
+                        "{s:0.8}we're adding {s:0.8,C:attention}Tenna{s:0.8} to it :3"
+                    },
+                    {
+                        "{s:0.9}Click 5 times to go to our {s:0.9,E:worm_elle_text}website",
+                        "{s:0.6}No, it won't count down. There's no {s:0.6,X:lfc_dark,C:white}loc_vars{s:0.6} on these.",
+                        "{s:0.6}The site is also very outdated rn but it'll be done later",
+                        "{s:0.6}This works on all 4 devs btw, check it out!"
+                    }
+                }
+            },
+
+            PotatoPatchDev_j8bit = {
+                name = "J8-Bit",
+                text = {
+                    {
+                        "{s:1.5}So... {C:purple,s:1.5}chunks{s:1.5}, huh?"
+                    },
+                    {
+                        "{s:0.9}I made most of the artwork,",
+                        "{s:0.9}the shader effects for",
+                        "{C:legendary,s:0.9}Moon Berry{s:0.9}, {C:uncommon,s:0.9}Golden Record{s:0.9},",
+                        "{s:0.9}and {s:0.9,C:common}Magical Girl{s:0.9},",
+                        "{s:0.9}and a good chunk of the",
+                        "{s:0.9}programming/design work"
+                    },
+                    {
+                        "If you're interested in",
+                        "more of my stuff:",
+                        "{s:0.8,X:blue,C:white}Bluesky{s:0.8} @j8-bit.bsky.social",
+                        "{s:0.8,X:red,C:white}YouTube{s:0.8} @j8-bitforager842",
+                        "{s:0.8,X:lfc_dark,C:white}Tumblr{s:0.8} @aforager",
+                        "{s:0.8}Play the {s:0.8,C:attention}Forager Nonessentials{s:0.8} mod!",
+                        "{s:0.8}Play CalvinChess on {s:0.8,X:lfc_dark,C:white}Steam{s:0.8}!",
+                        "{s:0.6}Click 5 times to open the {s:0.6,X:lfc_dark,C:white}Steam{s:0.6} page"
+                    }
+                }
+            },
+
+            PotatoPatchDev_alexi = {
+                name = "Alexi!",
+                text = {
+                    {
+                        "{s:1.5,E:worm_alexi_text}Alexi!",
+                        "the weird and deranged",
+                        "shapeshifting slimegirl!",
+                        "{s:0.5}see {C:blue,s:0.5}https://en.pronouns.page/@invalidOS",
+                        "{s:0.5}for pronouns and links!",
+                        "just click me 5 times!",
+                    },
+                    {
+                        "{s:0.8}I came up with a few of the ideas here,",
+                        "{s:0.8}my favorites being {C:uncommon,s:0.8}Urination Station{s:0.8} and {C:rare,s:0.8}Spacebar{s:0.8}!",
+                        "{s:0.8}I also coded {C:uncommon,s:0.8}Urination Station{s:0.8}, {C:uncommon,s:0.8}Stakataka{s:0.8}, and a few other things!",
+                        "{C:attention,s:0.8}The Fleet{s:0.8} was entirely done by me, and",
+                        "{s:0.8}the background of {C:attention,s:0.8,E:1}Eigengrau Emptiness{s:0.8} is some of",
+                        "{s:0.8}my proudest work yet! {s:0.5}(even if others got the same idea...)",
+                    },
+                    {
+                        "lemniscate",
+                        "{C:dark_edition,E:1}tomorrow"
+                    },
+                }
+            }
+        },
+
+        Blind = {
+            bl_worm_lfc_fleet = {
+                name = "舰队",
+                text = {
+                    "抽牌后，",
+                    "摧毁一张手牌中的",
+                    "随机卡牌",
+                    "+1手牌上限"
+                },
+            },
+            bl_worm_lfc_eigengrau = {
+                name = "灰暗虚空",
+                text = {
+                    "每张被修改的牌被抽出时",
+                    "有 #1# / #2# 概率",
+                    "变为石头牌"
+                },
+            }
+        },
+
+        Joker = {
+            j_worm_lfc_meteor_shower = {
+                name = "流星雨",
+                text = {
+                    "打出的计分牌会引发{C:lfc_meteor,E:1}流星{}",
+                    "坠落在屏幕上",
+                    "点击后给予{C:money}#1##2#{}"
+                }
+            },
+            j_worm_lfc_pissstream = {
+                name = "排尿站",
+                text = {    
+                    "此小丑根据国际空间站",
+                    "{C:chips}水{}、{C:mult}废物{}和{C:money}尿液{}储罐",
+                    "各自的{C:attention}百分比填充度{}（向上取整），",
+                    "获得{C:chips}+#1#{}筹码、{C:mult}+#2#{}倍率和{C:money}+$#3#{}金钱",
+                    "{C:inactive}(当前为{C:chips}+#4#{C:inactive}筹码，{C:mult}+#5#{C:inactive}倍率，{C:money}+$#6#{C:inactive})"
+                }
+            },
+            j_worm_lfc_log = {
+                name =
+                {
+                    "小丑日志",
+                    "{s:0.5,C:inactive}星历 #4#"
+                },
+                text = {
+                    {
+                        "{C:inactive,s:0.8}#3#天无撞击事件",
+                        "在本局游戏中",
+                        "每获得过{C:attention}一种{}不同的小丑，就给予{C:chips}+#1#{}筹码",
+                        "{C:inactive}(当前为{}{C:chips}+#2#{} {C:inactive}筹码){}",
+                    }
+
+                }
+            },
+            j_worm_lfc_blacephalon = {
+                name = {
+                    "究极异兽",
+                    "{s:0.5}砰头小丑"
+                },
+                text = {
+                    {
+                        "每张被{C:red}摧毁{}的",
+                        "牌给予{C:money}$#1#{}",
+                    },
+                    {
+                        "{s:0} " -- this is where the dex entry gets added!
+                    }
+                }
+            },
+            j_worm_lfc_stakataka = {
+                name = {
+                    "究极异兽",
+                    "{s:0.5}垒磊石"
+                },
+                text = {
+                    {
+                        "手牌中持有的{C:attention}#1#s{}",
+                        "会被加入打出的{C:attention}牌型{}中"
+                    },
+                    {
+                        "{s:0} " -- this is where the dex entry gets added!
+                    }
+                }
+            },
+            j_worm_lfc_brain_scrambler = {
+                name = {
+                    "心灵干扰器"
+                },
+                text = {
+                    "重新触发并{C:attention,E:1}打乱",
+                    "所有打出的牌",
+                }
+            },
+            j_worm_lfc_golden_record = {
+                name = {
+                    "黄金唱片"
+                },
+                text = {
+                    "每弃掉{C:red}#3#{}{C:inactive}[#4#]{}{C:attention}#1#s{}",
+                    "创建一张{V:1}#2#{}牌",
+                    "{C:inactive}(必须有空间)"
+                }
+            },
+            j_worm_lfc_galaxy_fantasy = {
+                name = "银河幻想",
+                text = {
+                    "经过{C:attention}#1#{}轮后，",
+                    "出售此牌以创建",
+                    "一张{C:legendary,E:1}#3#{}的复制",
+                    "{C:inactive}(必须有空间)",
+                    "{C:inactive}(当前为{C:attention}#2#{C:inactive}/#1#)",
+                },
+            },
+            j_worm_lfc_fw = {
+                name = {
+                    "月之浆果",
+                    "{E:1,C:inactive,s:0.5}哇噢"
+                },
+                text = {
+                    {
+                        "打出的{C:attention}#1#s{}",
+                        "在计分时提供{X:mult,C:white} X#2# {}倍率",
+                    },
+                    {
+                        "{C:inactive,s:0.85}#3#{}",
+                        "{C:inactive,s:0.85}#4#{}",
+                    }
+                }
+            },
+            j_worm_lfc_magical_girl = {
+                name = "魔法少女",
+                text = {
+                    "每当一张扑克牌",
+                    "获得新的{C:attention}强化{}时，",
+                    "此小丑获得{C:chips}+#1#{}筹码",
+                    "{C:inactive}(当前为{C:chips}+#2#{C:inactive}筹码)"
+                }
+            },
+            j_worm_lfc_spacebar = {
+                name = "空格键",
+                text = {
+                    {
+                        "在计分前进行{C:attention}时机小游戏{}",
+                        "每成功命中一次，获得{C:white,X:mult}X#1#"
+                    },
+                    {
+                        "在正确的时机按下{C:attention}空格{}",
+                        "以获得{C:white,X:mult}X倍率{}",
+                        "计分后重置"
+                    }
+                }
+            },
+            j_worm_lfc_astronaut_food = {
+                name = "航空食品",
+                text = {
+                    "接下来的{C:attention}#1#{}次出牌",
+                    "在{C:attention}升级{}时额外获得",
+                    "{C:chips}+#2#{}筹码和{C:mult}+#3#{}倍率"
+                },
+            },
+            j_worm_lfc_hitchhiker = {
+                name = {
+                    "小丑银河系",
+                    "漫游指南",
+                },
+                text = {
+                    "如果打出的计分牌包含",
+                    "一张{C:attention}#2#{}和一张{C:attention}#3#{}",
+                    "则从{V:2,B:1}#1#{}模组中",
+                    "创建一张小丑牌",
+                    "{C:inactive}(必须有空间)",
+                }
+            }
+        },
+        Spectral = {
+            c_worm_lfc_dark_matter = {
+                name = "暗物质",
+                text = {
+                    "摧毁手牌中",
+                    "{C:attention}#1#{}-{C:attention}#2#{}张随机卡牌",
+                    "每摧毁一张卡牌",
+                    "创建{C:attention}#3#{}个随机标签",
+                },
+            },
+            c_worm_lfc_time_dilation = {
+                name = "时间膨胀",
+                text = {
+                    "{C:attention}#1#{}底注,",
+                    "{C:attention}#2#{}个获胜底注,",
+                    "{C:red}摧毁{}并{C:red}禁用{}一张",
+                    "你拥有的随机{C:attention}小丑{}",
+                    "{C:inactive,s:0.75}(被禁用的小丑在",
+                    "{C:inactive,s:0.75}本局游戏中不再出现)"
+                },
+            },
+            c_worm_lfc_wish = {
+                name = "许愿",
+                text = {
+                    "给予手牌中",
+                    "{C:attention}1{}张选中的牌",
+                    "添加{C:lfc_meteor}流星蜡封{}",
+                },
+            }
+        },
+        Tag = {
+            tag_worm_lfc_laser = {
+                name = "激光标签",
+                text = {
+                    "{C:red}摧毁{}牌组中",
+                    "所有的{C:attention}#1#{}",
+                    "{C:inactive,s:0.8}(点数随机)"
+                },
+            },
+        },
+        Other = {
+            worm_lfc_meteor_seal = {
+                name = "流星蜡封",
+                text = {
+                    "计分时，",
+                    "创建一个{ C:attention}可点击的",
+                    "{C:lfc_meteor}流星{}",
+                    "命中后给予{C:money}$#1#{}"
+                }
+            }
+        }
+    },
+    misc = {
+        dictionary = {
+            k_lfc_none = "无",
+            k_lfc_secret1 = "出售此小丑以揭示一个秘密！",
+            k_lfc_secret2 = "(创建一个月之浆果的额外复制，需要有空间)",
+            k_lfc_secret3 = "上当了吧，哈哈",
+            k_lfc_secret4 = "哇，好秘密哦",
+        },
+        v_dictionary = {
+            lfc_dex_blacephalon_us = {
+                "{s:0.75}来自 {B:1,C:white,s:0.75}宝可梦 究极之日{s:0.75}：",
+                "{s:0.75}它悄悄靠近人。然后，在没有警告的情况下，",
+                "{s:0.75}引爆自己的头。它 apparently",
+                "{s:0.75}是一种 {s:0.75,C:dark_edition,E:1}究极异兽{}。",
+            },
+            lfc_dex_blacephalon_um = {
+                "{s:0.75}来自 {B:1,C:white,s:0.75}宝可梦 究极之月{s:0.75}：",
+                "{s:0.75}一只从 {s:0.75,C:dark_edition,E:1}究极之洞{s:0.75} 出现的 {s:0.75,C:dark_edition,E:1}UB{s:0.75}，",
+                "{s:0.75}它引发爆炸，然后利用",
+                "{s:0.75}对手的惊讶来夺取他们的活力。"
+            },
+            lfc_dex_stakataka_us = {
+                "{s:0.75}来自 {B:1,C:white,s:0.75}宝可梦 究极之日{s:0.75}：",
+                "{s:0.75}它从 {s:0.75,C:dark_edition,E:1}究极之洞{s:0.75} 出现。每一只",
+                "{s:0.75}似乎都是由许多生命形式",
+                "{s:0.75}一个接一个堆叠而成。"
+            },
+            lfc_dex_stakataka_um = {
+                "{s:0.75}来自 {B:1,C:white,s:0.75}宝可梦 究极之月{s:0.75}：",
+                "{s:0.75}当石墙开始移动并攻击时，",
+                "{s:0.75}这个怪物的真实身份是这个神秘的",
+                "{s:0.75}生命形式，让人联想到 {s:0.75,C:dark_edition,E:1}究极异兽{s:0.75}。"
+            },
+            lfc_obtain_pokemon_warning = {
+                "{s:0.75}获得此 {C:attention,s:0.75}小丑{s:0.75} 以",
+                "{s:0.75}解锁其 {C:dark_edition,s:0.75}宝可梦图鉴条目{s:0.75}！",
+            },
+        },
+        labels = {
+            worm_lfc_meteor_seal = "流星蜡封"
+        }
+    }
+}
