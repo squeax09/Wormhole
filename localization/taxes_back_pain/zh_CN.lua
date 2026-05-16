5529fe47cbbeb4f4a4c4e0f05064e8b6bad022ac
tzerolalala <131914786+tzerolalala@users.noreply.github.com>
2026-05-16 14:27:39 +0800

    Chinese translation
    Chinese translation
    I translated all the new content, except for the team introduction :)

---
+++ localization/taxes_back_pain/zh_CN.lua
@@ -0,0 +1,533 @@
+local other_loc_table = {
+    misc = {
+        labels = {
+            tbp_module = "模块"
+        },
+        dictionary = {
+            b_tbp_module_cards = "模块牌",
+            k_tbp_spaceship = "宇宙飞船",
+            k_tbp_module = "模块",
+            k_worm_tbp_module = "模块包",
+            tbp_module_weapons = '武器',
+            tbp_module_core = '核心',
+            tbp_module_thrusters = '推进器',
+            tbp_module_utility = '实用',
+            tbp_undisc_module = '未发现模块',
+            tbp_module_equip = '装备',
+            tbp_module_store = '商店',
+            tbp_new_module = '新模块',
+            tbp_old_module = '当前已装备',
+            tbp_equipped = ' 已装备！',
+            tbp_durability_restored = '已修复！',
+            k_tbp_name_Spaceship = "模块化宇宙飞船",
+            k_tbp_name_Vaianu = "瓦伊阿努 XI",
+            k_tbp_name_Explorer = "探索者号",
+            k_tbp_name_Artemis = "阿尔忒弥斯号",
+            k_tbp_name_Galactica = "银河号",
+            k_tbp_name_Sealab = "海研实验室银河旅行号",
+            k_tbp_name_Eremillenium = "埃雷米伦尼姆猎鹰",
+        }
+    },
+    descriptions = {
+        PotatoPatch = {
+            PotatoPatchTeam_tbp = { name = 'Taxes and Back Pain' },
+            PotatoPatchDev_eremel = {
+                name = 'Eremel',
+                text = {
+                    {
+                        'Look at this {C:worm_eremel}silly UI{} I made (:',
+                        '{C:inactive,s:0.6}I did some backend stuff too'
+                    },
+                    {
+                        'I help make {C:attention,E:2}SMODS{}!'
+                    },
+                    {
+                        'I also worked on {C:green,E:2}Galdur{},',
+                        '{C:blue,E:2}Malverk{}, {C:red,E:2}Ortalab{} and {C:gold,E:2}Monarchy{}'
+                    }
+                }
+            },
+            PotatoPatchDev_n = {
+                name = 'N\'',
+                text = {
+                    {
+                        "i coded"
+                    },
+                    {
+                        "{C:inactive}[Excerpt from Electronic Gaming Monthly Issue 15 interview (1990)]{}",
+                        "{C:attention}\"So, we had this idea, right?",
+                        "{C:attention}What if... just what if... we made a spaceship for",
+                        "{C:attention}the space event! Everyone thought we were out of our minds! (laughs)\"{}",
+                        " ",
+                        "{C:blue}\"Interesting. And was there any other idea",
+                        "{C:blue}you had in mind at the time?\"{}",
+                        " ",
+                        "{C:attention}\"No.\"{}"
+                    },
+                    {
+                        "Also worked on:",
+                        "{C:green}JokerDisplay{}, {C:hearts}JoyousSpring",
+                        "{C:purple}N's Repertorium{}, {C:blue}VanillaRemade"
+                    }
+                }
+            },
+            PotatoPatchDev_sdm = {
+                name = 'SDM_0',
+                text = {
+                    {
+                        'I am WAY too underqualified',
+                        'to be on this awesome team'
+                    },
+                    {
+                        'Some of my mods include {C:attention,E:2}SDM_0\'s Stuff{},',
+                        '{C:money,E:2}Joker Evolution{} and {C:red,E:2}Make-A-Tro{}'
+                    }
+                }
+            },
+            PotatoPatchDev_dilly = {
+                name = 'DillyTheDillster',
+                text = {
+                    {
+                        'I joined this event specifically for this team',
+                        "I did the original framework for the module dialog, as well",
+                        "as the shader for it among other things! I love these guys",
+                        "and I beg you to check out each of their mods, they're all fantastic!"
+                    },
+                    {
+                        'I also work on {C:worm_dilly}Dilatro{} primarily, soon to be released',
+                        "Featuring 3D blinds and more!"
+                    }
+                }
+            },
+            PotatoPatchDev_ice = {
+                name = 'Ice',
+                text = {
+                    {
+                        'I thought I was going to be doing',
+                        "some programming or art",
+                        "but my team was so talented",
+                        "that I was mostly an ideas guy"
+                    },
+
+                    {
+                        "I made {C:attention}Glue For Modpacks{},",
+                        "{C:mult}Blockbuster{} & {C:worm_ice,E:2}Balatro Goes Kino"
+                    }
+                }
+            },
+            PotatoPatchDev_rsnow = {
+                name = 'RattlingSnow',
+                text = {
+                    {
+                        '"Jack of all trades, master of none. though oftentimes better than a master of one"'
+                    }
+                }
+            },
+            PotatoPatchDev_mythie = {
+                name = 'itsmythie',
+                text = {
+                    {
+                        'Creator and Artist',
+                        'of Monarchy'
+                    },
+                    {
+                        'the shit i went through to be here',
+                        '{s:4,tbp_img:worm_tbp_cyclone}a{}'
+                    }
+                }
+            },
+        },
+        Back = {
+            b_worm_spaceship_deck = {
+                name = "宇宙飞船卡组",
+                text = {
+                    "开局时带有一艘{T:j_worm_tbp_spaceship,C:attention}模块化宇宙飞船{}",
+                    "它在每次{C:attention}商店{}开始时",
+                    "总会生成一个{T:p_worm_module_jumbo_1,C:tbp_module}巨型{}{T:p_worm_module_jumbo_1,C:tbp_module}模块包{}"
+                },
+            },
+        },
+        Other = {
+            p_worm_module_normal_1 = {
+                name = "模块包",
+                text = {
+                    {
+                        "从{C:attention}#2#{}张",
+                        "{C:tbp_module}模块{}牌中选择{C:attention}#1#{}张"
+                    },
+                    {
+                        "打开时，",
+                        "如果未拥有任何{C:attention}模块化宇宙飞船{}",
+                        "则创建一艘"
+                    }
+                },
+            },
+            p_worm_module_normal_2 = {
+                name = "模块包",
+                text = {
+                    {
+                        "从{C:attention}#2#{}张",
+                        "{C:tbp_module}模块{}牌中选择{C:attention}#1#{}张"
+                    },
+                    {
+                        "打开时，",
+                        "如果未拥有任何{C:attention}模块化宇宙飞船{}",
+                        "则创建一艘"
+                    }
+                },
+            },
+            p_worm_module_normal_3 = {
+                name = "模块包",
+                text = {
+                    {
+                        "从{C:attention}#2#{}张",
+                        "{C:tbp_module}模块{}牌中选择{C:attention}#1#{}张"
+                    },
+                    {
+                        "打开时，",
+                        "如果未拥有任何{C:attention}模块化宇宙飞船{}",
+                        "则创建一艘"
+                    }
+                },
+            },
+            p_worm_module_normal_4 = {
+                name = "模块包",
+                text = {
+                    {
+                        "从{C:attention}#2#{}张",
+                        "{C:tbp_module}模块{}牌中选择{C:attention}#1#{}张"
+                    },
+                    {
+                        "打开时，",
+                        "如果未拥有任何{C:attention}模块化宇宙飞船{}",
+                        "则创建一艘"
+                    }
+                },
+            },
+            p_worm_module_jumbo_1 = {
+                name = "巨型模块包",
+                text = {
+                    {
+                        "从{C:attention}#2#{}张",
+                        "{C:tbp_module}模块{}牌中选择{C:attention}#1#{}张"
+                    },
+                    {
+                        "打开时，",
+                        "如果未拥有任何{C:attention}模块化宇宙飞船{}",
+                        "则创建一艘"
+                    }
+                },
+            },
+            p_worm_module_jumbo_2 = {
+                name = "巨型模块包",
+                text = {
+                    {
+                        "从{C:attention}#2#{}张",
+                        "{C:tbp_module}模块{}牌中选择{C:attention}#1#{}张"
+                    },
+                    {
+                        "打开时，",
+                        "如果未拥有任何{C:attention}模块化宇宙飞船{}",
+                        "则创建一艘"
+                    }
+                },
+            },
+            p_worm_module_mega_1 = {
+                name = "超级模块包",
+                text = {
+                    {
+                        "从{C:attention}#2#{}张",
+                        "{C:tbp_module}模块{}牌中选择{C:attention}#1#{}张"
+                    },
+                    {
+                        "打开时，",
+                        "如果未拥有任何{C:attention}模块化宇宙飞船{}",
+                        "则创建一艘"
+                    }
+                },
+            },
+            p_worm_module_mega_2 = {
+                name = "超级模块包",
+                text = {
+                    {
+                        "从{C:attention}#2#{}张",
+                        "{C:tbp_module}模块{}牌中选择{C:attention}#1#{}张"
+                    },
+                    {
+                        "打开时，",
+                        "如果未拥有任何{C:attention}模块化宇宙飞船{}",
+                        "则创建一艘"
+                    }
+                },
+            },
+
+            -- Undiscovered Modules
+            undiscovered_tbp_module = {
+                name = '未发现',
+                text = {
+                        "在一场没有种子的流程中",
+                        "购买这张牌来查看它的作用",
+                }
+            },
+            
+        },
+        Tag = {
+            tag_worm_tbp_rocketry = {
+                name = "火箭技术标签",
+                text = {
+                    "给予一个免费的",
+                    "{C:tbp_module}超级模块包"
+                }
+            }
+        }
+    },
+}
+
+local descriptions = {
+    Joker = {
+        spaceship = {
+            name = '#2#',
+            text = {
+                '{V:1}当没有装备任何模块时，',
+                '{V:1}在商店中添加{V:2}#1#',
+                '{V:1}'
+            }
+        },
+        spaceship_back = {
+            name = '#2#',
+            text = {
+                '{V:1}在商店中添加{V:2}#1#',
+                '{V:1}'
+            }
+        },
+    },
+    tbp_module = {
+        module_missing = {
+            name = '名称',
+            text = {
+                '{V:1}空'
+            }
+        },
+        -- CORE --
+        nebula = {
+            name = "星云核心",
+            text = {
+                '当{C:tbp_module}模块{}被{C:attention}耗尽{}时，',
+                '随机提升一种牌型',
+                '{C:attention}#1#{}级',
+                '{s:0.8,C:inactive}(触发后耗尽)'
+            }
+        },
+        astrophage = {
+            name = "噬星体核心",
+            text = {
+                '使用{C:planet}星球{}牌',
+                '将使随机一个{C:tbp_module}模块{}',
+                '的耐久度提高{C:attention}#1#{}',
+                '{s:0.8,C:inactive}(触发后耗尽)'
+            }
+        },
+        dellinger = {
+            name = "戴林杰核心",
+            text = {
+                '打出{C:attention}#2#{}牌型',
+                '将使其他{C:tbp_module}模块{}',
+                '的耐久度提高{C:attention}#1#{}',
+                '{s:0.8,C:inactive}(每回合耗尽)'
+            }
+        },
+        black_hole_generator = {
+            name = "黑洞发生器",
+            text = {
+                '当{C:tbp_module}模块{}被{C:attention}耗尽{}时，',
+                '使其他{C:tbp_module}模块{}',
+                '的耐久度提高{C:attention}#1#{}',
+                '{s:0.8,C:inactive}(触发后耗尽)'
+            }
+        },
+        -- WEAPON --
+        void = {
+            name = '虚空射线',
+            text = {
+                '摧毁本回合{C:attention}首次出牌{}中打出的随机牌，',
+                '每摧毁一张，',
+                '增加{C:purple}#1#%{}的所需分数',
+                '{s:0.8,C:inactive}(每摧毁一张牌耗尽一次)'
+            }
+        },
+        ballistics = {
+            name = '先进弹道学',
+            text = {
+                "每张打出的{C:attention}牌{}",
+                "在计分时永久获得",
+                "{C:chips}+#1#{}筹码",
+                '{s:0.8,C:inactive}(每张牌计分时耗尽一次)'
+            }
+        },
+        waste = {
+            name = '武器化废物处理系统',
+            text = {
+                '本局中每耗尽一个{C:tbp_module}模块{}',
+                '提供{C:mult}+#1#{}倍率',
+                '{C:inactive}(当前{C:mult}+#2#{C:inactive}倍率)',
+                '{s:0.8,C:inactive}(每次出牌耗尽)'
+            }
+        },
+        salvo = {
+            name = '过载齐射',
+            text = {
+                '若打出的计分牌包含{C:attention}5{}张牌，',
+                '则每装备一个{C:tbp_module}模块{}提供',
+                '{X:mult,C:white}X#1#{}倍率',
+                '{C:inactive}(当前为{X:mult,C:white}X#2#{C:inactive}倍率)',
+                '{s:0.8,C:inactive}(触发时耗尽所有模块)'
+            }
+        },
+        -- UTILITY --
+        hardlight = {
+            name = '硬光屏障',
+            text = {
+                '选择{C:attention}盲注{}时，',
+                '将其要求降低{C:red}#1#%{}',
+                '{s:0.8,C:inactive}(每回合耗尽)'
+            }
+        },
+        quantum = {
+            name = '量子计算机',
+            text = {
+                '当其他{C:green}概率{}事件成功时，',
+                '获得{C:money}$#1#{}',
+                '{s:0.8,C:inactive}(触发时有{s:0.8,C:green}#2# / #3#{s:0.8,C:inactive}概率耗尽自身)'
+            }
+        },
+        interference = {
+            name = '干扰矩阵',
+            text = {
+                '禁用选中的',
+                '{C:attention}Boss盲注{}',
+                '{s:0.8,C:inactive}(触发后耗尽)'
+            }
+        },
+        redundancy = {
+            name = '冗余电池',
+            text = {
+                '当某个{C:tbp_module}模块{}即将',
+                '{C:attention}耗尽{}时，',
+                '改为由本模块消耗{C:attention}#1#{}点耐久',
+            }
+        },
+        -- THRUSTERS --
+        warp_drive = {
+            name = '曲速引擎',
+            text = {
+                '跳过{C:attention}盲注{}时，',
+                '获得{X:mult,C:white}X#1#{}倍率',
+                '{C:inactive}(当前为{X:mult,C:white}X#2#{C:inactive})',
+                '{C:inactive,s:0.8}(击败盲注时耗尽)'
+            }
+        },
+        temporal_jump = {
+            name = '时空跳跃推进器',
+            text = {
+                '跳过{C:attention}盲注{}时，',
+                '获得{C:attention}#1#{}个随机{C:attention}标签{}',
+                '{C:inactive,s:0.8}(跳过盲注时耗尽)'
+            }
+        },
+        repeater = {
+            name = '中继引擎',
+            text = {
+                '每张计分的',
+                '{C:attention}强化{}牌',
+                '打出时{C:attention}额外触发两次{}',
+                '{s:0.8,C:inactive}(每次出牌耗尽)'
+            }
+        },
+        antimatter = {
+            name = '反物质推进器',
+            text = {
+                '为打出的牌',
+                '创建一张{C:dark_edition}负片{}{C:planet}星球{}牌',
+                '{s:0.8,C:inactive}(每次出牌耗尽)'
+            }
+        },
+        -- hyperlight_rotors = {
+        --     name = '超光速转子',
+        --     text = {
+        --         '每个空的 {C:attention}小丑槽位{}',
+        --         '提供 {X:mult,C:white} X#1# {C:mult} 倍率',
+        --         '{C:inactive}(当前 {X:mult,C:white}X#2#{C:inactive})',
+        --         '{C:inactive,s:0.8}计分时受损'
+        --     }
+        -- },
+        -- hyperlight_rotors_equipped = {
+        --     name = '{C:attention}超光速转子',
+        --     text = {
+        --         '{s:1.2,V:1}超光速转子{}',
+        --         '每个空的 {C:attention}小丑槽位{}',
+        --         '提供 {X:mult,C:white} X#1# {C:mult} 倍率',
+        --         '{C:inactive}(当前 {X:mult,C:white}X#2#{C:inactive})',
+        --         '{C:inactive,s:0.8}计分时耗尽'
+        --     }
+        -- },
+        -- resuited_thruster = {
+        --     name = '换色推进器',
+        --     text = {
+        --         '打出的 {V:1}#2#{} 花色牌',
+        --         '提供 {C:chips}+#1#{} 筹码',
+        --         '{C:inactive,s:0.8}计分时受损',
+        --         '{C:inactive,s:0.8}花色每回合结束时变化'
+        --     }
+        -- },
+        -- resuited_thruster_equipped = {
+        --     name = '{C:attention}换色推进器',
+        --     text = {
+        --         '{s:1.2,V:1}换色推进器{}',
+        --         '打出的 {V:2}#2#{} 花色牌',
+        --         '提供 {C:chips}+#1#{} 筹码',
+        --         '{C:inactive,s:0.8}计分时耗尽',
+        --         '{C:inactive,s:0.8}花色每回合结束时变化'
+        --     }
+        -- },
+        -- uncat3 = {
+        --     name = '未分类3',
+        --     text = {
+        --         "每张打出的 {C:attention}牌{}",
+        --         "在计分时永久获得",
+        --         "{C:mult}+#1#{} 倍率",
+        --         '{s:0.8,C:inactive}(每张牌计分时耗尽一次)'
+        --     }
+        -- },
+        -- uncat3_equipped = {
+        --     name = '{C:attention}未分类3',
+        --     text = {
+        --         '{s:1.2,V:1}未分类3',
+        --         "每张打出的 {C:attention}牌{}",
+        --         "在计分时永久获得",
+        --         "{C:mult}+#1#{} 倍率",
+        --         '{s:0.8,C:inactive}(每张牌计分时耗尽一次)'
+        --     }
+        -- },
+        -- uncat4 = {
+        --     name = '未分类4',
+        --     text = {
+        --         "当 {C:tbp_module}模块{}",
+        --         "被 {C:attention}耗尽{} 时，",
+        --         "获得 {C:money}+$#1#{}",
+        --         '{s:0.8,C:inactive}(每次触发时耗尽自身)'
+        --     }
+        -- },
+        -- uncat4_equipped = {
+        --     name = '{C:attention}未分类4',
+        --     text = {
+        --         '{s:1.2,V:1}未分类4',
+        --         "当 {C:tbp_module}模块{}",
+        --         "被 {C:attention}耗尽{} 时，",
+        --         "获得 {C:money}+$#1#{}",
+        --         '{s:0.8,C:inactive}(每次触发时耗尽自身)'
+        --     }
+        -- },
+    }
+}
+
+return Wormhole.tbp.prefix_loc_table(descriptions, other_loc_table)
