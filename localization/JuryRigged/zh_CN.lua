175bd9ac6641bedd7b7cc114bead0dd05111f585
tzerolalala <131914786+tzerolalala@users.noreply.github.com>
2026-05-16 14:20:02 +0800

    Add files via upload
    Chinese translation
    I translated all the new content, except for the team introduction :)

---
+++ localization/JuryRigged/zh_CN.lua
@@ -0,0 +1,329 @@
+return {
+  descriptions = {
+    worm_jr_satellite = {
+      sat_worm_new_horizon = {
+        name = "新视野号",
+        text = {
+          {
+            "将{C:attention}#2#{}的",
+            "{C:worm_jr_satellite}卫星{}升级至{S:0.8}({S:0.8,V:1}等级#1#{S:0.8}){}",
+          },
+          {
+            "打出{C:attention}#2#{}后，",
+            "额外抽{C:attention}#1#{}张牌"
+          }
+        }
+      },
+      sat_worm_messenger = {
+        name = "信使号",
+        text = {
+          {
+            "将{C:attention}#2#{}的",
+            "{C:worm_jr_satellite}卫星{}升级至{S:0.8}({S:0.8,V:1}等级#1#{S:0.8}){}",
+          },
+          {
+            "若打出的牌型是{C:attention}#2#{}",
+            "{C:attention}未计分{}的牌有{C:green}#3# / #4#{}概率",
+            "提供{C:money}$#1#{}"
+          }
+        }
+      },
+      sat_worm_tianwen_4 = {
+        name = "天问四号",
+        text = {
+          {
+            "将{C:attention}#2#{}的",
+            "{C:worm_jr_satellite}卫星{}升级至{S:0.8}({S:0.8,V:1}等级#1#{S:0.8}){}",
+          },
+          {
+            "若打出的牌型",
+            "是{C:attention}#2#{}",
+            "复制一张随机计分牌",
+            "{C:attention}#1#{}次"
+          }
+        }
+      },
+      sat_worm_venera_9 = {
+        name = "金星九号",
+        text = {
+          {
+            "将{C:attention}#2#{}的",
+            "{C:worm_jr_satellite}卫星{}升级至{S:0.8}({S:0.8,V:1}等级#1#{S:0.8}){}",
+          },
+          {
+            "若打出的牌型",
+            "是{C:attention}#2#{}",
+            "牌堆中{C:attention}#1#{}张牌",
+            "各有{C:green}#4# / #5#{}概率",
+            "变为{C:attention}#2#{}的点数"
+          }
+        }
+      },
+      sat_worm_cassini_huygens = {
+        name = "卡西尼-惠更斯号",
+        text = {
+          {
+            "将{C:attention}#2#{}的",
+            "{C:worm_jr_satellite}卫星{}升级至{S:0.8}({S:0.8,V:1}等级#1#{S:0.8}){}",
+          },
+          {
+            "若打出的牌型是{C:attention}#2#{}",
+            "有{C:green}#4# / #5#{}概率摧毁牌堆中",
+            "不属于{C:attention}#2#{}牌型的",
+            "相同点数的牌",
+            "{C:attention}#1#{}次"
+          }
+        }
+      },
+      sat_worm_galileo = {
+        name = "伽利略号",
+        text = {
+          {
+            "将{C:attention}#2#{}的",
+            "{C:worm_jr_satellite}卫星{}升级至{S:0.8}({S:0.8,V:1}等级#1#{S:0.8}){}",
+          },
+          {
+            "若打出的牌型是{C:attention}#2#{}",
+            "有{C:green}#4# / #5#{}概率将牌堆中",
+            "一张牌的花色变为",
+            "{C:attention}#2#{}中最常见的花色",
+            "{C:attention}#1#{}次"
+          }
+        }
+      },
+      sat_worm_sputnik_1 = {
+        name = "斯普特尼克一号",
+        text = {
+          {
+            "将{C:attention}#2#{}的",
+            "{C:worm_jr_satellite}卫星{}升级至{S:0.8}({S:0.8,V:1}等级#1#{S:0.8}){}",
+          },
+          {
+            "若打出的牌型",
+            "是{C:attention}#2#{}",
+            "对牌组中{C:attention}#1#{}张牌",
+            "随机添加一种{C:attention}强化{}、{C:attention}版本{}",
+            "或{C:attention}蜡封{}"
+          }
+        }
+      },
+      sat_worm_mariner_9 = {
+        name = "水手九号",
+        text = {
+          {
+            "将{C:attention}#2#{}的",
+            "{C:worm_jr_satellite}卫星{}升级至{S:0.8}({S:0.8,V:1}等级#1#{S:0.8}){}",
+          },
+          {
+            "若打出的牌型是",
+            "{C:attention}#2#{}，牌组中每张",
+            "与{C:attention}#2#{}点数相同的牌",
+            "提供{C:money}$#1#{}"
+          }
+        }
+      },
+      sat_worm_voyager_2 = {
+        name = "旅行者二号",
+        text = {
+          {
+            "将{C:attention}#2#{}的",
+            "{C:worm_jr_satellite}卫星{}升级至{S:0.8}({S:0.8,V:1}等级#1#{S:0.8}){}",
+          },
+          {
+            "若打出的牌型",
+            "是{C:attention}#2#{}",
+            "计分牌中的牌",
+            "永久获得{C:white,X:mult}X#3#{}倍率"
+          }
+        }
+      },
+      sat_worm_death_egg = {
+        name = "死亡之蛋",
+        text = {
+          {
+            "将{C:attention}#2#{}的",
+            "{C:worm_jr_satellite}卫星{}升级至{S:0.8}({S:0.8,V:1}等级#1#{S:0.8}){}",
+          },
+          {
+            "若打出的牌型",
+            "是{C:attention}#2#{}",
+            "所有小丑牌提供{C:white,X:mult}X#3#{}倍率"
+          }
+        }
+      },
+      sat_worm_dawn = {
+        name = "黎明号",
+        text = {
+          {
+            "将{C:attention}#2#{}的",
+            "{C:worm_jr_satellite}卫星{}升级至{S:0.8}({S:0.8,V:1}等级#1#{S:0.8}){}",
+          },
+          {
+            "若打出的牌型",
+            "是{C:attention}#2#{}",
+            "最后{C:attention}2{}张打出的牌",
+            "计分时提供{C:white,X:mult}X#3#{}倍率"
+          }
+        }
+      },
+      sat_worm_manhole_cover = {
+        name = "井盖",
+        text = {
+          {
+            "将{C:attention}#2#{}的",
+            "{C:worm_jr_satellite}卫星{}升级至{S:0.8}({S:0.8,V:1}等级#1#{S:0.8}){}",
+          },
+          {
+            "若打出的牌型",
+            "是{C:attention}#2#{}",
+            "随机打出的牌触发{C:attention}#1#{}次"
+          }
+        }
+      },
+    },
+    Voucher = {
+      v_worm_jr_launch_pad = {
+        name = '发射台',
+        text = {
+          "{C:planet}天体包{}中增加{C:attention}#1#{}张",
+          "{C:worm_jr_satellite}卫星{}牌"
+        }
+      },
+      v_worm_jr_flyby = {
+        name = '飞掠',
+        text = {
+          "使用{C:worm_jr_satellite}卫星{}牌时",
+          "为其{C:attention}对应的牌型{}",
+          "创建一张{C:dark_edition}负片{}{C:planet}星球牌{}"
+        }
+      },
+    },
+    Back = {
+      b_worm_jr_lunar = {
+        name = "月球卡组",
+        text = {
+          "{C:worm_jr_satellite}卫星{}牌可能",
+          "出现在商店中，",
+          "开局时拥有",
+          "{C:worm_jr_satellite,T:v_worm_jr_launch_pad}发射台{}优惠券"
+        }
+      },
+    },
+    Joker = {
+      j_worm_jr_transponder = {
+        name = "应答器",
+        text = {
+          "每拥有1个{C:worm_jr_satellite}卫星{}牌",
+          "获得{X:mult,C:white}X#1#{}倍率",
+          "{C:inactive}(当前为{X:mult,C:white}X#2#{C:inactive}倍率)",
+        }
+      },
+      j_worm_jr_nasa = {
+        name = "NASA",
+        text = {
+          "击败{C:attention}Boss盲注{}后，",
+          "为你{C:attention}最后一次打出的牌型{}",
+          "创建对应的{C:worm_jr_satellite}卫星{}牌"
+        }
+      },
+      j_worm_jr_deep_space_probe = {
+        name = "深空探测器",
+        text = {
+          "牌型每获得{C:attention}#2#{}级",
+          "创建一张{C:worm_jr_satellite}卫星{}牌",
+          "{C:inactive}(当前为{C:attention}#1#{C:inactive}/{C:inactive}#2#)",
+        }
+      },
+      j_worm_jr_crash_course = {
+        name = "速成班",
+        text = {
+          "若打出牌型时手牌中有",
+          "对应的{C:planet}星球牌{}",
+          "摧毁该{C:planet}星球牌{}并",
+          "创建其对应的{C:worm_jr_satellite}卫星{}牌"
+        }
+      },
+    },
+    Other = {
+      p_worm_jr_orbital_normal = {
+        name = "轨道包",
+        text = {
+          "从{C:attention}#2#{}张",
+          "{C:worm_jr_satellite}卫星{}牌中选择{C:attention}#1#{}张",
+          "立即使用"
+        }
+      },
+      p_worm_jr_orbital_jumbo = {
+        name = "巨型轨道包",
+        text = {
+          "从{C:attention}#2#{}张",
+          "{C:worm_jr_satellite}卫星{}牌中选择{C:attention}#1#{}张",
+          "立即使用"
+        }
+      },
+      p_worm_jr_orbital_mega = {
+        name = "超级轨道包",
+        text = {
+          "从{C:attention}#2#{}张",
+          "{C:worm_jr_satellite}卫星{}牌中选择{C:attention}#1#{}张",
+          "立即使用"
+        }
+      },
+      undiscovered_worm_jr_satellite = {
+        name = "未发现",
+        text = {
+          "在一场没有种子的流程中",
+          "购买这张牌来查看它的作用",
+        },
+      },
+    },
+    Tag = {
+      tag_worm_jr_planetarium = {
+        name = "天文馆标签",
+        text = {
+          "获得一个免费的",
+          "{C:worm_jr_satellite}超级轨道包"
+        }
+      },
+    },
+    PotatoPatch = {
+      PotatoPatchTeam_JuryRigged = { name = "JuryRigged" },
+      PotatoPatchDev_DowFrin = { name = "DowFrin", text = { { "Listen to {E:2,C:red}Fire4Fun{} by {C:money}Jhariah{} :7", "Credit to {C:chips}Inky{} for this credit art" }, { "I coded the framework + utils for the {E:2,C:worm_jr_satellite}Satellite{} cards,", "the vouchers and most of the jokers", }, { "All things considered this team was pretty amazing to work with!" } } },
+      PotatoPatchDev_Maelmc = { name = "Maelmc", text = { "Play {C:attention}The Binding of Jimbo{} :)" } },
+      PotatoPatchDev_Inky = { name = "Inky", text = { { "Did most of the {C:worm_jr_satellite}satellite{} art and drew some joker art" }, { "(...yes, the {C:chips}Square{} Joker guy. That's him.)" } } },
+      PotatoPatchDev_DoggFly = { name = "Dogg-Fly", text = { "i love yuri, yuri is my life" } },
+      PotatoPatchDev_AbelSketch = { name = "AbelSketch", text = { { "{C:attention}Fun Fact:{} Theres a reason to not share water bottles!" }, { "I got Salmonela from one... 3:" } } },
+      PotatoPatchDev_Blanthos = { name = "Blanthos", text = { "First we Gneep, then we Gnarp. Lets {C:green}Gneep Gnarp{}." } },
+      PotatoPatchDev_NinjaBanana = { name = "NinjaBanana", text = { "GUYS I'M NOT THE IMPOSTOR PLEASE DON'T EJECT ME" } },
+    }
+  },
+  misc = {
+    dictionary = {
+      -- Standard badge text
+      k_worm_jr_satellite = "卫星",
+      -- For Death Egg and Manhole
+      k_worm_jr_satellite_fake = "卫星..?",
+
+      b_worm_jr_satellites = "卫星",
+      b_worm_jr_satellite_cards = "卫星牌",
+
+      worm_jr_orbital_pack = "轨道包",
+
+      -- Satellite names for Run Info
+      worm_jr_satellites_new_horizon = "新视野号",
+      worm_jr_satellites_messenger = "信使号",
+      worm_jr_satellites_tianwen_4 = "天问四号",
+      worm_jr_satellites_venera_9 = "金星九号",
+      worm_jr_satellites_galileo = "伽利略号",
+      worm_jr_satellites_cassini_huygens = "卡西尼-惠更斯号",
+      worm_jr_satellites_sputnik_1 = "斯普特尼克一号",
+      worm_jr_satellites_mariner_9 = "水手九号",
+      worm_jr_satellites_voyager_2 = "旅行者二号",
+      worm_jr_satellites_death_egg = "死亡之蛋",
+      worm_jr_satellites_dawn = "黎明号",
+      worm_jr_satellites_manhole_cover = "井盖",
+
+      worm_jr_plus_satellite = "+1卫星",
+    }
+  }
+}
