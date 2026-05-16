a88b660bc1700be832c03b99038cd4b19c835021
tzerolalala <131914786+tzerolalala@users.noreply.github.com>
2026-05-16 14:23:25 +0800

    Chinese translation
    Chinese translation
    I translated all the new content, except for the team introduction :)

---
+++ localization/awesomeswagmoney/zh_CN.lua
@@ -0,0 +1,231 @@
+return {
+    descriptions = {
+        Back = {},
+        Blind = {},
+        Edition = {},
+        Enhanced = {},
+        Joker = {
+            j_worm_necrozma = {
+                name = '奈克洛兹玛',
+                text = { "当击败一个{C:attention}boss盲注{}后", "创建一个免费的{C:attention}#1#{}" }
+            }
+        },
+        Other = {
+            undiscovered_worm_ultrabeast = {
+                name = "未发现",
+                text = {
+                    "在一场没有种子的流程中",
+                    "购买这张牌来查看它的作用"
+                }
+            },
+            p_worm_wormhole_normal = {
+                name = "微型究极之洞",
+                text = {
+                    "从{C:attention}#2#个{C:worm_ultrabeast}究极异兽{}中",
+                    "选择{C:attention}#1#{}个"
+                }
+            },
+            p_worm_wormhole_jumbo = {
+                name = "究极之洞",
+                text = {
+                    "从{C:attention}#2#个{C:worm_ultrabeast}究极异兽{}中",
+                    "选择{C:attention}#1#{}个"
+                }
+            },
+            p_worm_wormhole_mega = {
+                name = "超级究极之洞",
+                text = {
+                    "从{C:attention}#2#个{C:worm_ultrabeast}究极异兽{}中",
+                    "选择{C:attention}#1#{}个"
+                }
+            }
+        },
+        Planet = {},
+        Spectral = {
+            c_worm_ultramegaopolis = {
+                name = "究极大都会",
+                text = {
+                    "释放{C:worm_necrozma_r,E:1}究极之黑"
+                }
+            }
+        },
+        Stake = {},
+        Tag = {
+            tag_worm_ub = {
+                name = '究极异兽标签',
+                text = {
+                    "获得一个免费的",
+                    "{C:worm_ultrabeast}超级究极之洞"
+                }
+            }
+        },
+        Tarot = {},
+        Voucher = {},
+        PotatoPatch = {
+            PotatoPatchTeam_awesomeswagmoney = {name = "Team Awesomeswagmoney"},
+            PotatoPatchDev_worm_garb = {
+                name = "Garb",
+                text = {
+                    {"Hi!!!! I'm Garb", "I made GARBSHIT"},
+                    {
+                        'Play my game Onibi when it comes out in uhhh',
+                        '2000 years'
+                    }, {'I fucking love Xurkitree'}
+                }
+            },
+            PotatoPatchDev_worm_poker = {
+                name = "Poker The Poker",
+                text = {"{C:legendary,E:1}:chud:"}
+            },
+            PotatoPatchDev_worm_omega = {name = "Omegaflowey18", text = {
+                {
+                    "Artist, resident joker shipper",
+                    "Artist for Hot Potato, BU Language Mod, and guest artist on Garbshit",
+                    "If you see Perkeo kissing Triboulet anywhere, it's probably my fault",
+                },
+                {
+                    "I fucking love Blacephalon"
+                }
+            }},
+            PotatoPatchDev_worm_superb = {
+                name = "Superb Thing",
+                text = {
+                    "Hello, I'm Superb Thing",
+                    "I exist, apparently",
+                    'I made a mod called "Electrum"',
+                    "That's all, I think"
+                }
+            },
+            PotatoPatchDev_worm_eris = {name = "Eris", text = {
+                {"we outta ultra wormholes"},
+                {
+                    "Mod dev for {C:spectral}Spectrallib{} and {C:spectral}Cryptid{},",
+                    "and my solo-dev mod {C:planet}Hypernova{}.",
+                    "You might also have seen me",
+                    "in previous {C:attention}Potato Patch{} events"
+                },
+                {
+                    "I fucking love the hit videogame",
+                    "{C:attention}Risk of Rain 2{}, and also {C:worm_ultrabeast}Nihilego"
+                }
+            }}
+        },
+        worm_ultrabeast = {
+            c_worm_pheromosa = {
+                name = '费洛美螂',
+                text = {
+                    "下次出牌给予{C:chips}+#1#{}筹码",
+                    "计分后消耗"
+                }
+            },
+            c_worm_buzzwole = {
+                name = '爆肌蚊',
+                text = {
+                    "下次出牌给予{C:mult}+#1#{}倍率",
+                    "计分后消耗"
+                }
+            },
+            c_worm_xurkitree = {
+                name = "电束木",
+                text = {
+                    "获得{C:money}$#1#{},然后永久",
+                    "增加下一个{C:spectral}质数{}的数量"
+                }
+            },
+            c_worm_kartana = {
+                name = "纸御剑",
+                text = {
+                    "摧毁{C:attention}1{}张选中牌,",
+                    "添加{C:attention}#1#{}张永久复制到你的牌组",
+                    "它们的点数减少{C:attention}#2#",
+                    "然后抽取它们",
+                    "{C:inactive,s:0.8}(点数不能小于2)"
+                }
+            },
+            c_worm_blacephalon = {
+                name = "砰头小丑",
+                text = {
+                    "给予随机一张{C:attention}手牌{}{C:dark_edition}多彩{}",
+                    "然后摧毁其他手牌",
+                }
+            },
+            c_worm_celesteela = {
+                name = "铁火辉夜",
+                text = {
+                    "一回合增加{C:attention}+#1#{}手牌上限,然后",
+                    "永久增加下一个{C:spectral}质数{}的数量"
+                }
+            },
+            c_worm_guzzlord = {
+                name = "恶食大王",
+                text = {
+                    "摧毁{C:attention}#1#{}张随机手牌",
+                    "升级{C:attention}最常打出",
+                    "{C:attention}牌型{}{C:attention}#2#{}级"
+                }
+            },
+            c_worm_stakataka = {
+                name = "垒磊石",
+                text = {
+                    "将{C:attention}#1#{}张选中牌",
+                    "变为{C:dark_edition}多彩{}{C:attention}石头{}牌",
+                }
+            },
+            c_worm_naganadel = {
+                name = "四颚针龙",
+                text = {
+                    "将一张随机{C:attention}小丑{}",
+                    "变为{C:dark_edition}永恒{}和{C:dark_edition}多彩{}",
+                }
+            },
+            c_worm_poipole = {
+                name = "毒贝比",
+                text = {
+                    "将一张随机{C:attention}小丑{}变为{C:dark_edition}永恒,",
+                    "然后{C:attention}3{}张随机手牌",
+                    "变为{C:dark_edition}镭射{}",
+                }
+            },
+            c_worm_nihilego = {
+                name = "虚吾伊德",
+                text = {
+                    "接下来的{C:attention}2{}回合给予{X:mult,C:white} X#1# {}倍率",
+                    "{C:attention}1{}张手牌{C:red}总是",
+                    "{C:red}被选中{}"
+                }
+            }
+        }
+    },
+    misc = {
+        achievement_descriptions = {},
+        achievement_names = {},
+        blind_states = {},
+        challenge_names = {},
+        collabs = {},
+        dictionary = {
+            k_worm_ultrabeast = "究极异兽",
+            b_worm_ultrabeast_cards = "究极异兽牌",
+            k_worm_ultrawormhole = "究极之洞",
+            k_worm_necrozma_r = "究极大都会",
+            k_asm_necrozmaspawn = "虫洞!"
+        },
+        high_scores = {},
+        labels = {
+            worm_ultrabeast = "究极异兽",
+            k_worm_necrozma_r = "究极大都会",
+        },
+        poker_hand_descriptions = {},
+        poker_hands = {},
+        quips = {
+            worm_kartana_jumpscare = { --reverse jetstream sam jumpscare
+                "?tsal uoy nac gnol woH"
+            }
+        },
+        ranks = {},
+        suits_plural = {},
+        suits_singular = {},
+        tutorial = {},
+        v_dictionary = {},
+        v_text = {},
+    },
+}
