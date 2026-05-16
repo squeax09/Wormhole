592fc8b725a7105e7930c85338c47159862ff77e
tzerolalala <131914786+tzerolalala@users.noreply.github.com>
2026-05-16 14:19:03 +0800

    Add files via upload
    Chinese translation
    I translated all the new content, except for the team introduction :)

---
+++ localization/Dummies/zh_CN.lua
@@ -0,0 +1,355 @@
+return {    -- yo it's GHOSTSALT here look at me TYPING before i SUBMIT THIS THING because i wanna leave one more MARK on this MOD before i SUBMIT THIS THING
+    descriptions = {
+        PotatoPatch = {
+            PotatoPatchTeam_dummies = { name = 'Dummies' },
+            PotatoPatchDev_ghostsalt = {
+                name = 'GhostSalt',
+                text = {
+                    { "Hiya! :3" },
+                    { "Made: Moony, Gleebleglorp, Crystal Sphere", "and Star-Studded Deck" },
+                    { "Additional art: Garlic Bread that", "went to Space, Tim Curry,", "Mouthwash and TOGA's card below" },
+                    { "I also animated some of", "the things we made ^u^" },
+                    { "Have fun with the mod!! :D" }
+                }
+            },
+            PotatoPatchDev_vissa = {
+                name = 'vissa',
+                text = {
+                    { "Made {X:green,C:white}Aliens{} that {C:red}eat{} stuff.", "Also did half of the {C:attention}Test Flight{} blind", "and some art."},
+                    { "Please tell me other", "teams added {X:chips,C:white}Fish{} jokers...", "{C:inactive,s:0.9}my carnivore is gonna starve" },
+                    { "{C:red}Eats{} cards while", "you're not looking.", "{C:inactive,s:0.9}glorpalicious"}
+                }
+            },
+            PotatoPatchDev_bagels = {
+                name = 'BakersDozenBagels',
+                text = { "gwah" }
+            },
+            PotatoPatchDev_theonegoofali = {
+                name = 'TheOneGoofAli',
+                text = {
+					{ "Additionally known under the {C:attention}TOGA{} acronym.", "Very much interested in (old) computers.", "{C:inactive}And older versions of Windows too." },
+					{ "Previously modded {C:attention}Sonic Robo Blast 2{},", "{C:attention}Sonic Robo Blast 2 Kart{} and {C:attention}Dr. Robotnik's Ring Racers{}." },
+					{ "Responsible for appearance of {C:edition}the SMODS cat{} on loading screen since", "{C:attention}BETA-1201a{}, as well as the {C:legendary}custom{} loading screen for this mod.", "{C:inactive}Originally experimented for TOGA's Stuff{}", "{C:inactive,s:0.9}(https://github.com/TheOneGoofAli/TOGAPackBalatro){}" }
+				}
+            },
+            PotatoPatchDev_baltdev = {
+                name = 'baltdev',
+                text = { "hi im a slugcat" }
+            },
+            PotatoPatchDev_flowire = {
+                name = 'Flowire',
+                text = {
+                    { "Awwooo!~ :3" },
+                    { "I hope your day", "is as wonderful", "as you are! {C:red}<3" },
+                    { "This {C:attention}critter{}", "tastes like a", "{C:red}Strawberry{}!" },
+                }
+            },
+        },
+        Back = {
+            b_worm_dum_starstudded = {
+                name = "群星云集卡组",
+                text = {
+                    "{C:red}-1{}{C:attention}小丑牌槽位{}",
+                    "累计使用{C:attention}9{}张不同的获得{C:planet}星球{}牌",
+                    "之后{C:attention}+2小丑牌槽位{}"
+                }
+            }
+        },
+        Joker = {
+            j_worm_dum_worm = {
+                name = "格利斯特先生",
+                text = {
+                    "所有补充包{C:attention}免费{}.",
+                    "当你没在看时,{C:red}吃掉{}所有补充包"
+                },
+            },
+            j_worm_dum_greg = {
+                name = "咀嚼者格雷格",
+                text = {
+                    "在一张手牌计分后{C:red}吃掉{}{C:attention}最右侧{}的牌",
+                    "并为{C:attention}最左侧{}的牌变为{C:blue}天体{}",
+                    "{C:red}无法出售",
+                },
+            },
+            j_worm_dum_carnivore = {
+                name = "食肉者莫库",
+                text = { {
+                    "当{C:attention}盲注{}被选中时",
+                    "右侧的{C:attention}小丑{}如果是{X:chips,C:white}鱼{},",
+                    "{X:grey,C:white}猫{}或{X:green,C:white}外星人{}, {C:red}吃掉{}它",
+                    "然后永久给予{C:chips}+#2#{}筹码, {C:mult}+#1#{}倍率或",
+                    "{X:mult,C:white}X#3#{}倍率",
+                }, {
+                    "{C:inactive}当前为{C:chips}+#5#{C:inactive}筹码,",
+                    "{C:mult}+#4#{C:inactive}倍率和{X:mult,C:white}X#6#{C:inactive}倍率"
+                } },
+            },
+            j_worm_dum_garlicbread = {
+                name = { "想去{C:planet}太空{}的{C:worm_dum_garlicbread}蒜香面包{}" },
+                text = {
+                    "使用{C:attention}#2#{}张{C:planet}星球{}牌后",
+                    "升级{C:attention}所有{}牌型",
+                    "{C:inactive}(当前为{C:attention}#1#{C:inactive}/#2#){}"
+                },
+            },
+            j_worm_dum_farfromhome = {
+                name = "远征",
+                text = { {
+                    "每当使用一张{C:planet}星球{}牌",
+                    "存储{X:chips,C:white}X#1#{}筹码",
+                    "{C:inactive}(已存储{X:chips,C:white}X#2#{C:inactive}筹码){}"
+                }, {
+                    "使用一张{C:planet}地球{}释放",
+                    "所有存储的筹码,",
+                    "每次出牌失去{X:chips,C:white}X#3#{}筹码",
+                    "{C:inactive}(当前为{X:chips,C:white}X#4#{C:inactive}筹码){}"
+                } },
+            },
+            j_worm_dum_gleebleglorp = {
+                name = "格里布洛普", -- <3
+                text = {
+                    "如果打出的{C:attention}牌型{}",
+                    "不是你{C:attention}等级最高{}的牌型",
+                    "{C:white,X:mult}X#1#{}倍率"
+                },
+            },
+			j_worm_dum_crystalsphere = {
+                name = "水晶球",
+                text = {
+                    "如果打出的牌包含{C:attention}人头{}牌",
+                    "{C:green}#1# / #2#{}概率{C:attention}重新触发{}所有计分牌"
+                },
+            },
+			j_worm_dum_scrooge = {
+                name = "{C:worm_dum_scrooge}鸭子传说{}: {C:planet}新月{}",
+                text = {
+                    "打出持有{C:planet}星球{}牌{C:attention}对应{}的",
+                    "牌型{}时,获得{C:money}#1#{}"
+                },
+            },
+            j_worm_dum_spaghet = {
+                name = "{C:worm_dum_spaghet}意大利面化",
+                text = {
+                    "如果你{C:attention}弃掉{}持有{C:planet}星球{}牌",
+                    "对应的牌型,{C:red}摧毁{}那张{C:planet}星球{}牌",
+                    "给予对应{C:attention}牌型等级{}的{C:chips}筹码{}",
+                    "{C:inactive}(当前为{C:chips}+#1#{C:inactive}筹码)",
+                },
+            },
+			j_worm_dum_timcurry = {
+                name = "{C:worm_dum_timcurry}蒂姆·克里{}",
+                text = { {
+					"如果打出的牌型是包含一对{C:attention}Q{}的{C:attention}对子{}",
+					"提升{C:attention}随机{}牌型{C:planet}#1#{}级",
+				}, {
+					"{C:inactive}有概率变为太空小丑"
+				} },
+            },
+            j_worm_dum_pentapod = {
+                name = "五足生物",
+                text = {
+                    "如果{C:attention}计分牌{}包含{C:attention}#2#{}张牌",
+                    "在计分{C:attention}之前{}给予{X:mult,C:white}X#1#{}倍率"
+                }
+            },
+            j_worm_dum_mouthwash = {
+                name = "漱口水",
+                text = {
+                    "当{C:attention}盲注{}被选中时,",
+                    "{C:attention}降级{}一种随机牌型",
+					"然后给予{X:mult,C:white}X#1#{}倍率",
+                    "{C:inactive}(当前为{X:mult,C:white}X#2#{C:inactive}倍率)",
+                },
+            },
+        },
+        Planet = {
+            c_worm_dum_moony = {
+                name = "似月",
+                text = {
+                    "从{C:attention}#1#{}种随机{C:attention}牌型{}",
+                    "种选择一种提升{C:attention}#2#{}级"
+                }
+            },
+        },
+        Spectral = {
+            c_worm_dum_pulsar = {
+                name = "脉冲星",
+                text = {
+                    "本回合{C:attention}最常用牌型{}的",
+                    "的{C:chips}筹码{}和{C:mult}倍率{}成长{C:attention}翻倍{}",
+                    "然后将其牌型等级设为{C:attention}1",
+                    "{C:inactive}(当前为{C:attention}#1#{C:inactive})"
+                }
+            }
+        },
+        Blind = {
+            bl_worm_dum_dummy_blind = {
+                name = "试飞",
+                text = {
+                    "你不在这个盲注输掉",
+                    "获得的{C:attention}奖励{}基于",
+                    "你的分数",
+                    "{s:0.8,C:worm_dum_brown}让我们看看你能不能",
+                    "{s:0.8,C:worm_dum_brown}驾驶宇宙飞船!"
+                },
+            },
+        },
+        Voucher = {
+            v_worm_dum_spacesuit = {
+                name = "宇航服",
+                text = { {
+                    "{C:attention}给予{}并{C:attention}增加{}{C:attention}#1#{}分钟",
+                    "你的最大{C:spectral}氧气值{}",
+                }, {
+                    "击败一个{C:attention}盲注{}后{C:attention}#2#{} seconds of {C:spectral}Oxygen{}",
+                    "给予{C:attention}#2#{}秒{C:spectral}氧气值{}",
+                }, {
+                    "倍乘等同于你{C:spectral}氧气值{}",
+                    "一半的{C:chips}筹码{}和{C:mult}倍率{}",
+                    "{C:inactive,s:0.8}(例如. {C:attention,s:0.8}2:53{C:inactive,s:0.8} -> {X:purple,C:white,s:0.8}X1.27{C:inactive,s:0.8}倍率和筹码){}",
+                    "{C:inactive,s:0.8}(倍乘无法低于{X:purple,C:white,s:0.8}X1{C:inactive,s:0.8}){}",
+                } }
+            },
+            v_worm_dum_oxygentank = {
+                name = "氧气罐",
+                text = { {
+                    "{C:attention}给予{}并{C:attention}增加{}{C:attention}#1#{}分钟",
+                    "你的最大{C:spectral}氧气值{}",
+                }, {
+                    "击败一个{C:attention}盲注{}后",
+                    "{u:inactive}减少{}{C:attention}#2#{}秒{C:spectral}氧气值{}获取",
+                } }
+            },
+        },
+        Tag = {
+            tag_worm_dum_trinary = {
+                name = "三进制系统",
+                text = {
+                    "当{C:attention}盲注{}被选中后",
+                    "创建一张{C:planet}星球{},{C:spectral}幻灵{}或{C:tarot}塔罗{}牌",
+                    "{C:inactive}(必须有空间){}",
+                    "{C:inactive}(剩余{C:attention}#1#{C:inactive}){}次",
+				}
+			},
+			tag_worm_dum_timcurry = {
+				name = "蒂姆·克里标签",
+				text = {
+					"商店出现一张{C:worm_dum_timcurry}蒂姆·克里{}?!",
+				},
+			},
+        },
+        Other = {
+            worm_dum_spacesuit_warning = {
+                name = "{s:1.2}警告{}",
+                text = {
+                    "当{C:spectral}氧气值{}耗尽",
+                    "{C:red,E:2}输掉{E:2}游戏",
+                }
+            },
+            worm_dum_spacesuit_upgrade = {
+                name = "氧气升级",
+                text = {
+                    "{C:inactive}最大氧气值:", "{C:spectral}#1#{} -> {C:spectral}#2#{}",
+                    "{s:0.5} ",
+                    "{C:inactive}恢复时间:", "{C:attention}#3#{} -> {C:attention}#4#{}",
+                }
+            },
+        },
+        Edition = {
+            e_worm_dum_Celestial = {
+                name = '天体',
+                text = {
+                    "当使用一张{C:planet}星球{}牌",
+                    "{C:green}#1# / #2#{} 概率升级一种",
+                    "{C:attention}随机{}牌型"
+                }
+            },
+        }
+    },
+    misc = {
+        labels = {
+            worm_dum_Celestial = '天体',
+        },
+        dictionary = {
+            k_worm_dum_worm_free = "免费补充包!",
+            k_worm_dum_worm_eat = "Gulp!",
+            k_worm_dum_dummy_blind_saved = "试飞失败!",
+            k_worm_dum_greg_eat = "Nom!",
+            k_worm_dum_carnivore_eat = "Chomp!",
+            worm_moony_menu_tooltip = {
+                "从下列选择一种",
+                "牌型进行升级"
+            },
+            k_worm_dum_low_oxygen = "含氧量低",
+			worm_tim_curry_space = "太空空空!",
+            k_worm_dum_tasty = "美味!",
+        },
+        v_dictionary = {
+            worm_dum_store = "+X#1#储存",
+            worm_dum_apply = "+X#1#施加",
+            worm_dum_percent = "#1#%",
+            worm_dum_xgeneric = "X#1#",
+            worm_dum_a_joker_slots = '+#1#小丑牌槽位',
+        },
+        challenge_names = {
+            c_worm_dum_low_oxygen = "含氧量低",
+            c_worm_dum_buff_spacesuit = "强化宇航服",
+            c_worm_dum_all_star_champion = "全明星",
+        },
+        v_text = {
+            ch_c_worm_dum_low_oxygen_1 = { "开局时拥有{C:attention}20{}分钟{C:spectral}氧气值{},", },
+            ch_c_worm_dum_low_oxygen_2 = { "{C:spectral}氧气值{}不提供计分", },
+            ch_c_worm_dum_buff_spacesuit_1 = { "享受{C:green,E:1}未削弱{}的{C:attention}太空服{}数值!", },
+            ch_c_worm_dum_buff_spacesuit_2 = { "{C:inactive}最大氧气值: {C:spectral}05:00{C:inactive} minutes", },
+            ch_c_worm_dum_buff_spacesuit_3 = { "{C:inactive}恢复时间: {C:attention}01:00{C:inactive} minute", },
+            ch_c_worm_dum_all_star_champion_1 = { "{C:attention,E:2}三进制{C:planet}星球{}牌", },
+            ch_c_worm_dum_all_star_champion_2 = { "{C:attention}+4{}底注", },
+            ch_c_worm_dum_gold_stake = { "应用{C:money}金注{}难度" },
+            ch_c_worm_dum_space = { "{s:0.5} ", },
+        },
+        quips = {
+            worm_dummies_win_1 = { "{s:2.0,C:green}胜" },
+            worm_dummies_win_2 = { "我就知道", "你能行！" },
+            worm_dummies_win_3 = { "小可爱们说“{E:1}啥{}”。" },
+            worm_dummies_win_4 = { "你真是个", "{C:blue,E:1}卡牌高手{}！" },
+            worm_dummies_win_5 = { "我把输掉的", "全赢回来了，谢谢！" },
+            worm_dummies_win_6 = { "又美{E:2}又{}聪明！" },
+            worm_dummies_win_7 = { "你是我的", "{C:red}3{}，我是你的{C:red}<{}" },
+            worm_dummies_win_8 = { "{s:2.0,C:green}+2" },
+            worm_dummies_win_9 = { "有这种操作，", "我终于明白", "“{E:2}神之一手{}”是怎么来的了！" },
+            worm_dummies_win_10 = { "{s:2.0}:3" },
+            worm_dummies_win_11 = { "{s:2.0} 笑死", "{s:2.0}  |Î| ", "{s:1.0}  绝了", "{s:2.0}电影级" },
+            worm_dummies_loss_1 = { "{s:2.0,C:red}负" },
+            worm_dummies_loss_2 = { "嗯，你该", "再加把劲，", "你这{u:red}水平{}… {E:2}哎哟{}！" },
+            worm_dummies_loss_3 = { "   我觉得你在这儿搜", '    “{C:red}退出{}” 更容易成功！', "                   {C:inactive}|", " ", "                   {C:inactive}|", " ", "                   {C:inactive}|", " ", "                   {C:inactive}|", "                   {C:inactive} - - - - - - - - >" },
+            worm_dummies_loss_4 = { "我本想帮你，", "但现在实在", "不太方便。" },
+            worm_dummies_loss_5 = { "啊，可恶！" },
+            worm_dummies_loss_6 = { "{E:2}别瞎闹了。" },
+            worm_dummies_loss_7 = { "你确定你", "知道怎么玩吗？" },
+            worm_dummies_loss_8 = { "要不你去问问", "{E:2}废物GPT{} 吧。" },
+            worm_dummies_loss_9 = { "我觉得你应该", "再玩一遍", "{C:blue}教程{}。" },
+            worm_dummies_loss_10 = { "{s:2.0,C:red}-2" },
+            worm_dummies_loss_11 = { "时间到，", "{E:2}伙计{}！" },
+            worm_dummies_flowire_1 = { "错误" },
+            worm_dummies_flowire_2 = { "空值" },
+            worm_dummies_flowire_3 = { "{s:2.5} " },
+            worm_dummies_timcurry_win_1 = { "又一场胜利，指挥官……" },
+            worm_dummies_timcurry_win_2 = { "{C:attention,s:1.5}好！{}", "干得漂亮。" },
+            worm_dummies_timcurry_win_3 = { "出色的工作，指挥官……" },
+            worm_dummies_timcurry_win_4 = { "干得好，同志。" },
+            worm_dummies_timcurry_lose_normal = { "{C:hearts}真可惜……" },
+            worm_dummies_timcurry_lose_full = { "唉……你都走到这一步了，", "却在最后失败了。", "{E:1,C:hearts}真可惜……" },
+        },
+		achievement_names = {
+			ach_worm_dum_timcurry = '太空空空!',
+			ach_worm_dum_seriouscredit = '专门致谢',
+			ach_worm_dum_unstoppable = '不可阻挡的力量',
+		},
+		achievement_descriptions = {
+			ach_worm_dum_timcurry = '持有蒂姆·克里时获得太空小丑',
+			ach_worm_dum_timcurry_hidden = "一个未被资本主义腐蚀的地方...",
+			ach_worm_dum_seriouscredit = '点击Team Credits的任何人...很多下...',
+			ach_worm_dum_unstoppable = '达到10,000%，并真正攻克‘试飞’',
+		}
+    }
+}
